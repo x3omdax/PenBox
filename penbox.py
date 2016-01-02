f9138ecaf46316c0c524dd87c9cefb2a6acf0074
Fedy Wesleti <fedyweslati@gmail.com>
2016-01-03 00:04:19 +0100

    Update penbox.py
    changed 1 / 0 to yes or no questions

--- penbox.py
+++ penbox.py
@@ -39,7 +39,7 @@ Select from the menu:
 3 : Wireless Testing
 4 : Exploitation Tools
 5 : Sniffing & Spoofing
-6 : Exit
+99 : Exit
 
 """
     choice = input("selet a number :")
@@ -61,10 +61,9 @@ Select from the menu:
 #nmap function 
 def nmap():
     print("this step will download and install nmap ")
-    print("1 to accept / 0 to decline")
-    choice7 = input("1/0 :")
-    choice7 = int(choice7)
-    if choice7 ==1:
+    print("yes or no ")
+    choice7 = raw_input()
+    if choice7 == "yes" :
         os.system("wget https://nmap.org/dist/nmap-7.01.tar.bz2")
         os.system("bzip2 -cd nmap-7.01.tar.bz2 | tar xvf -")
         os.system("cd nmap-7.01")
@@ -72,7 +71,7 @@ def nmap():
         os.system("make")
         os.system("su root")
         os.system("make install")
-    elif choice7 ==0:
+    elif choice7 == "no" :
         info()
 ####################################
 #jboss-autopwn
@@ -84,23 +83,19 @@ def jboss():
     print ("")
     print (" this will install jboss-autopwn") 
     print ("usage : ./e.sh target_ip tcp_port ")
-    print("1 to accept / 0 to decline")
-    choice9 = input("1/0 :")
-    choice9 = int(choice9)
-    if choice9 ==1:
+    choice9 = raw_input("yes / no :")
+    if choice9 in yes:
         os.system("git clone https://github.com/SpiderLabs/jboss-autopwn.git"),sys.exit();
-    elif choice9 ==0:
+    elif choice9 in no:
         os.system('clear'); exp()
 #sqlmap 
 def sqlmap():
     print (" this will install sqlmap ")
     print ("usage : python sqlmap.py -h")
-    print("1 to accept / 0 to decline")
-    choice8 = input("1/0 :")
-    choice8 = int(choice8)
-    if choice8 ==1:
+    choice8 = input("yes or no :")
+    if choice8 in yes:
         os.system("git clone https://github.com/sqlmapproject/sqlmap.git sqlmap-dev")
-    elif choice8 ==0:
+    elif choice8 in no:
         os.system('clear'); info()
 
 #setoolkit 
@@ -110,34 +105,31 @@ def setoolkit():
     print(" allow you to make a believable attack quickly. SET is a product of TrustedSec, LLC  ")
     print("an information security consulting firm located in Cleveland, Ohio.")
     print("")
-    print("1 to accept / 0 to decline")
-    choiceset = input("1/0 :")
-    choiceset = int(choiceset)
-    if choiceset ==1:
+    choiceset = raw_input("y / n :")
+    if choiceset in yes:
         os.system("git clone https://github.com/trustedsec/social-engineer-toolkit.git");os.system("cd social-engineer-toolkit");os.system("python setup.py")
-    if choiceset ==0:
+    if choiceset in no:
         os.system("clear"); info()
 #cupp 
 def cupp():
     print("cupp is a password list generator ")
     print("Usage: python cupp.py -h")
-    print("1 to accept / 0 to decline")
-    choicecupp = input("1/0 :")
-    choicecupp= int(choicecupp)
-    if choicecupp ==1:
+    print("yes or now")
+    choicecupp = raw_input("y / n :")
+    
+    if choicecupp in yes:
         os.system("git clone https://github.com/Mebus/cupp.git");os.system("cd cupp")
-    elif choicecupp ==0:
+    elif choicecupp in no:
         os.system("clear"); passwd()
 #ncrack 
 def ncrack():
     print("A Ruby interface to Ncrack, Network authentication cracking tool.")
     print("requires : nmap >= 0.3ALPHA / rprogram ~> 0.3")
     print("1 to accept / 0 to decline")
-    choicencrack = input("1/0 :")
-    choicencrack= int(choicencrack)
-    if choicencrack==1:
+    choicencrack = raw_input("y / n :")
+    if choicencrack in yes:
         os.system("git clone https://github.com/sophsec/ruby-ncrack.git");os.system("cd ruby-ncrack");os.systemgem("install ruby-ncrack")
-    elif choicencrack==0:
+    elif choicencrack in no:
         os.system("clear"); passwd()
 #reaver
 def reaver():
@@ -145,18 +137,17 @@ def reaver():
     print(" WPS registrar PINs in order to recover WPA/WPA2 passphrases. It has been tested against a")
     print(") wide variety of access points and WPS implementations")
     print("1 to accept / 0 to decline")
-    creaver = input("1/0 :")
-    creaver = int(creaver)
-    if creaver==1:
+    creaver = input("y / n :")
+    if creaver in yes:
         os.system("apt-get -y install build-essential libpcap-dev sqlite3 libsqlite3-dev aircrack-ng pixiewps");os.system("git clone https://github.com/t6x/reaver-wps-fork-t6x.git");os.system("cd reaver-wps-fork-t6x");os.system("cd src/");os.system("./configure");os.system("make")
-    elif creaver==0:
+    elif creaver in no:
         os.system("clear"); wire()
 
 #####################################
 #information gathering function
 def info():
-    print("1 :nmap ")
-    print("3 :SET tool kit")
+    print("1 : nmap ")
+    print("3 : SET tool kit")
     print("99 :Go Back To Main Menu")
     choice2 = input("selet a number :")
     choice2 = int(choice2)
@@ -171,8 +162,8 @@ def info():
 ##########################
 #password attacks menu 
 def passwd():
-    print("1 :cupp ")
-    print("2 :Ncrack")
+    print("1 : cupp ")
+    print("2 : Ncrack")
     print("99:Back To Main Menu")
     choice3 = input("selet a number :")
     choice3 = int(choice3)
@@ -186,7 +177,7 @@ def passwd():
 ##########################
 #wireless attacks
 def wire():
-    print("1 :reaver ")
+    print("1 : reaver ")
     print("99: Go Back To The Main Menu")
     choice4 = input("selet a number :")
     choice4 = int(choice4)
@@ -197,9 +188,9 @@ def wire():
 ##########################
 #exploitation tools
 def exp():
-    print("1 :jboss-autopwn ")
-    print("2 :sqlmap")
-    print("99 :Go Back To Main Menu")
+    print("1 : jboss-autopwn ")
+    print("2 : sqlmap")
+    print("99 : Go Back To Main Menu")
     choice5 = input("selet a number :")
     choice5 = int(choice5)
     if choice5 ==2:
@@ -211,7 +202,7 @@ def exp():
 ###########################
 #sniffing tools
 def snif():
-    print("1 :Set Tool kit ")
+    print("1 : Set Tool kit ")
     print("99: Back To Main Menu")
     choice6 = input("selet a number :")
     choice6 = int(choice6)
@@ -221,12 +212,31 @@ def snif():
        os.system("clear"); menu()
 #end of menu 
 ##########################
-  #Check user ID 
+  #Check use OS
+def OS():
+    print(
+    """
+    Choose Operating System : 
+    1) Max OSX
+    2) Linux
+    3) Windows
+    """)
+    system = input(":")
+    system = str(system)
+    if system ==2:
+        root()
+    else :
+        menu()
 
-if os.getuid() != 0:
+############################
+#check root if linux 
+def root():
+    if os.getuid() != 0:
         print("Are you root? Please execute as root")
         exit() 
-menu() #show the main menu 
+#############################
+#begin :D 
+OS()
     
     
     
